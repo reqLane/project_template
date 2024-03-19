3275f8f1e5717682c88bfd6ee177a5299ae45f56
Vlad Marchenko <vlad5000191@gmail.com>
2024-03-19 21:27:09 +0200

    Docstrings for input.py, output.py

--- app/io/input.py
+++ app/io/input.py
@@ -1,12 +1,38 @@
 
 
 def console_input():
+    """
+    Asks the user for text input from console and returns it as a string.
+
+    Returns:
+        str. The input user provided.
+    """
     pass
 
 
-def read_file_python():
+def read_file_python(path_to_file):
+    """
+    Reads content of the specified file and returns it as a string.
+    Uses only python built-in methods.
+
+    Args:
+        path_to_file(str): Path to the file to be read.
+
+    Returns:
+        str. The content of the specified file.
+    """
     pass
 
 
-def read_file_pandas():
+def read_file_pandas(path_to_file):
+    """
+    Reads content of the specified file and returns it as a string.
+    Uses methods provided by pandas library.
+
+    Args:
+        path_to_file(str): Path to the file to be read.
+
+    Returns:
+        str. The content of the specified file.
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,12 +1,34 @@
 
 
-def console_output():
+def console_output(text):
+    """
+    Writes text provided to the console.
+
+    Args:
+        text (str): Text to write to the console.
+    """
     pass
 
 
-def write_file_python():
+def write_file_python(text, path_to_file):
+    """
+    Writes text provided to the specified file.
+    Uses only python built-in methods.
+
+    Args:
+        text (str): Text to write to the file.
+        path_to_file(str): Path to the specified file.
+    """
     pass
 
 
-def write_file_pandas():
+def write_file_pandas(text, path_to_file):
+    """
+    Writes text provided to the specified file.
+    Uses methods provided by pandas library.
+
+    Args:
+        text (str): Text to write to the file.
+        path_to_file(str): Path to the specified file.
+    """
     pass
