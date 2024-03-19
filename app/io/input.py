6a50574e8ae82af29b9775ff8ceda2a0e45c0f0d
Vlad Marchenko <vlad5000191@gmail.com>
2024-03-19 20:25:19 +0200

    Empty io functions

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def console_input():
+    pass
+
+
+def read_file_python():
+    pass
+
+
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def console_output():
+    pass
+
+
+def write_file_python():
+    pass
+
+
+def write_file_pandas():
+    pass
